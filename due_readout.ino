e70e441c619aae00d48acdf5a8a0dd3d1a060837
Andrew Peck <anp@ucla.edu>
2014-09-11 13:59:06 -0700

    Lots of debugging, and redesigned to print output to serial port. Some bug possibly in Ethernet Shield code is causing naughty behavior with the Ethernet card. This commit is working.

--- due_readout.ino
+++ due_readout.ino
@@ -1,42 +1,17 @@
-#include <SPI.h>
 #include <Ethernet.h>
+#include <SPI.h>
 
 #define X1_A    0U
 #define X2_A    1U
 #define Y1_A    2U
 #define Y2_A    3U
 
-#define X1_B    4U
-#define X2_B    5U
-#define Y1_B    6U
-#define Y2_B    7U
-
-#define X1_C    8U
-#define X2_C    9U
-#define Y1_C    10U
-#define Y2_C    11U
-
-#define X1_D    8U
-#define X2_D    9U
-#define Y1_D    10U
-#define Y2_D    11U
-
 #define X_1     0U
 #define X_2     1U
 #define Y_1     2U
 #define Y_2     4U
 
-#define SENSOR_A 0U
-#define SENSOR_B 1U
-#define SENSOR_C 2U
-#define SENSOR_D 2U
-
-#define CLKA    50U
-#define CLKB    51U
-#define CLKC    52U
-#define CLKD    52U
-
-#define WAIT_TIME 50U
+#define CLKA    22U
 
 /* Setup Ethernet */
 byte mac[]      = {
@@ -57,22 +32,9 @@ EthernetServer server = EthernetServer(23);
 /* Variable Initialization, for later..*/
 uint32_t command;
 
-int client_connected = 0;
-
-uint32_t evt_A = 0;
-uint32_t evt_B = 0;
-uint32_t evt_C = 0;
-uint32_t evt_D = 0;
-
-boolean trigA;
-boolean trigB;
-boolean trigC;
-boolean trigD;
-
-boolean measureA;
-boolean measureB;
-boolean measureC;
-boolean measureD;
+/*uint8_t testdata[]      = {
+  0xFF, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00, 0x11, 0xFF
+};*/
 
 void setup()
 {
@@ -80,220 +42,98 @@ void setup()
   Ethernet.begin(mac, ip, gateway, subnet);
   server.begin();
 
-  /* Configure Interrupt Pins*/
-  pinMode(CLKA, INPUT);
-  pinMode(CLKB, INPUT);
-  pinMode(CLKC, INPUT);
-  pinMode(CLKD, INPUT);
+  //pinMode(DAC0, OUTPUT);
+  //pinMode(DAC1, OUTPUT);
 
-  /* Attach Interrupt Routines to pcint pins */
-  attachInterrupt(CLKA, pcintA, CHANGE);
-  attachInterrupt(CLKB, pcintB, CHANGE);
-  attachInterrupt(CLKC, pcintC, CHANGE);
-  attachInterrupt(CLKD, pcintD, CHANGE);
+  /* Configure Interrupt Pins*/
+  pinMode(CLKA, INPUT);                     // configure as input
+  attachInterrupt(CLKA, interrupt, CHANGE); // enable interrupts on this pin
+  digitalWrite(CLKA, HIGH);                 // turn on a pullup resistor (could just as well be LOW)
+
+  // Set Resolution for analog Read and Write
+  analogReadResolution(12);
+  analogWriteResolution(12);
+
+  // Setup Serial Bus
+  SerialUSB.begin(115200);
+  //while (!SerialUSB.available());
+  //SerialUSB.print("Starting...\n");
 }
 
 void loop()
 {
-  /* Check if there's some data to read */
-  EthernetClient client = server.available();
-  if (client) {
-    client_connected = 1;
-    uint32_t tmp = client.read();
-    if (tmp >= 0)
-      command = tmp;
-  }
-  else
-    client_connected = 0;
 
-  /* Control of Channel A */
-  if (command & 0x1)
-    measureA = true;
-  else
-    measureA = false;
-
-  /* Control of Channel B */
-  if (command & 0x2)
-    measureB = true;
-  else
-    measureB = false;
-
-  /* Control of Channel C */
-  if (command & 0x4)
-    measureC = true;
-  else
-    measureC = false;
-
-  /* Control of Channel D */
-  if (command & 0x8)
-    measureD = true;
-  else
-    measureD = false;
-
-  /* Read Bias and Threshold Voltage Settings */
-  byte bias   = (command >> 16) & 0xFF;
-  byte thresh = (command >>  8) & 0xFF;
-
-  /* If Ch.A is enabled and a trigger was seen, take a measurement*/
-  if (trigA && measureA) {
-    if (evt_A > 0xFFFFFF) evt_A = 0;
-    ReadSensor(SENSOR_A, evt_A);
-    evt_A += 1;
-    trigA = false;
-  }
-
-  /* If Ch.B is enabled and a trigger was seen, take a measurement */
-  if (trigB && measureB) {
-    if (evt_B > 0xFFFFFF) evt_B = 0;
-    ReadSensor(SENSOR_B, evt_B);
-    evt_B += 1;
-    trigB = false;
-  }
-
-  /* If Ch.C is enabled and a trigger was seen, take a measurement */
-  if (trigC && measureC) {
-    if (evt_C > 0xFFFFFF) evt_C = 0;
-    ReadSensor(SENSOR_C, evt_C);
-    evt_C += 1;
-    trigC = false;
-  }
-
-  /* If Ch.D is enabled and a trigger was seen */
-  if (trigD && measureD) {
-    if (evt_D > 0xFFFFFF) evt_D = 0;
-    ReadSensor(SENSOR_D, evt_D);
-    evt_D += 1;
-    trigD = false;
-  }
-
-  /* Write threshold and bias settings to DAC */
-  analogWrite(DAC0, thresh);
-  analogWrite(DAC1, bias);
 }
 
 /* Routine to take a reading from a single sensor and transmit results through TCP/IP */
-void ReadSensor(byte sensor, uint32_t event)
+void ReadSensor(uint16_t &x1, uint16_t &x2, uint16_t &y1, uint16_t &y2)
 {
-  if (client_connected == 1) {
-    int x1 = analogRead(whichPin(sensor, X_1));
-    int x2 = analogRead(whichPin(sensor, X_2));
-    int y1 = analogRead(whichPin(sensor, Y_1));
-    int y2 = analogRead(whichPin(sensor, Y_2));
+  x1 = analogRead(X1_A);
+  x2 = analogRead(X2_A);
+  y1 = analogRead(Y1_A);
+  y2 = analogRead(Y2_A);
 
-    byte header0;
-    byte header1;
-    byte rsvd;
-    byte evtbyte0;
-    byte evtbyte1;
-    byte evtbyte2;
-    byte trailer0;
-    byte trailer1;
+  uint8_t header0;
+  uint8_t trailer0;
 
-    header0  = 0x55;
-    header1  = sensor & 0x3;
-    rsvd     = 0;
-    evtbyte0 = (event >> 16) & 0xFF;
-    evtbyte1 = (event >>  8) & 0xFF;
-    evtbyte2 = (event >>  0) & 0xFF;
-    trailer0 = 0;
-    trailer1 = 0xAA;
+  header0  = 0xFF;
+  trailer0 = 0xFF;
 
-    byte data [] = {
-      header0,          //0
-      header1,          //1
-      rsvd,             //2
-      evtbyte0,         //3
-      evtbyte1,         //4
-      evtbyte2,         //5
-      (x1 >> 4) & 0xFF, //6
-      (x1 >> 0) & 0xFF, //7
-      (x2 >> 4) & 0xFF, //8
-      (x2 >> 0) & 0xFF, //9
-      (y1 >> 4) & 0xFF, //10
-      (y1 >> 0) & 0xFF, //11
-      (y2 >> 4) & 0xFF, //12
-      (y2 >> 0) & 0xFF, //13
-      trailer0,         //14
-      trailer1          //15
-    };
-
-    byte crc = CRC8(data, 15);
-    data[14] = crc;
-
-    server.write(data, sizeof(data));
-  }
 }
 
 /* Pin Change Interrupt Routines */
-void pcintA() {
-  read_adc_interrupt(trigA);
-}
-
-void pcintB() {
-  read_adc_interrupt(trigB);
-}
-
-void pcintC() {
-  read_adc_interrupt(trigC);
-}
-
-void pcintD() {
-  read_adc_interrupt(trigD);
-}
-
-void read_adc_interrupt(boolean trig) {
+void interrupt() {
+  uint16_t x1;
+  uint16_t x2;
+  uint16_t y1;
+  uint16_t y2;
+  int sum_x1 = 0;
+  int sum_x2 = 0;
+  int sum_y1 = 0;
+  int sum_y2 = 0;
+  int start_time; 
+  int stop_time; 
+  int nsamples = 40;
+  
   static unsigned long last_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
-  if (interrupt_time - last_interrupt_time > 300)
-    delayMicroseconds(WAIT_TIME);
-  trig = true;
-}
-
-/* Returns the Pin ID for a given sensor and coordinate (e.g. sensor A X1 => X1_A)*/
-unsigned int whichPin(unsigned int sensor, unsigned int coordinate) {
-  if (sensor == SENSOR_A) {
-    if (coordinate == X_1) return X1_A;
-    if (coordinate == X_2) return X2_A;
-    if (coordinate == Y_1) return Y2_A;
-    if (coordinate == Y_2) return Y2_A;
-  }
-
-  if (sensor == SENSOR_B) {
-    if (coordinate == X_1) return X1_B;
-    if (coordinate == X_2) return X2_B;
-    if (coordinate == Y_1) return Y2_B;
-    if (coordinate == Y_2) return Y2_B;
-  }
-
-  if (sensor == SENSOR_C) {
-    if (coordinate == X_1) return X1_C;
-    if (coordinate == X_2) return X2_C;
-    if (coordinate == Y_1) return Y2_C;
-    if (coordinate == Y_2) return Y2_C;
-  }
-
-  if (sensor == SENSOR_D) {
-    if (coordinate == X_1) return X1_D;
-    if (coordinate == X_2) return X2_D;
-    if (coordinate == Y_1) return Y2_D;
-    if (coordinate == Y_2) return Y2_D;
-  }
-}
-
-//CRC-8 - based on the CRC8 formulas by Dallas/Maxim
-//code released under the therms of the GNU GPL 3.0 license
-byte CRC8(const byte *data, byte len) {
-  byte crc = 0x00;
-  while (len--) {
-    byte extract = *data++;
-    for (byte tempI = 8; tempI; tempI--) {
-      byte sum = (crc ^ extract) & 0x01;
-      crc >>= 1;
-      if (sum) {
-        crc ^= 0x8C;
-      }
-      extract >>= 1;
+  unsigned long interrupt_time = micros();
+  if (interrupt_time - last_interrupt_time > 100)
+  {
+    delayMicroseconds(50);
+    //start_time = micros();
+    for (int i = 0; i < nsamples; i++) {
+      ReadSensor(x1, x2, y1, y2);
+      sum_x1 += x1;
+      sum_x2 += x2;
+      sum_y1 += y1;
+      sum_y2 += y2;
+    //stop_time = micros(); 
     }
+    
+    char msg [50];
+    x1 = sum_x1 / nsamples;
+    x2 = sum_x2 / nsamples;
+    y1 = sum_y1 / nsamples;
+    y2 = sum_y2 / nsamples;
+
+    //sprintf(msg, "%04i", stop_time-start_time);
+    sprintf(msg, "%04i,%04i,%04i,%04i", x1, x2, y1, y2);
+    SerialUSB.println(msg);
   }
-  return crc;
 }
+
+/*
+  uint8_t data [] = {
+    header0,          //0
+    (x1 >> 8) & 0xFF, //1
+    (x1 >> 0) & 0xFF, //2
+    (x2 >> 8) & 0xFF, //3
+    (x2 >> 0) & 0xFF, //4
+    (y1 >> 8) & 0xFF, //5
+    (y1 >> 0) & 0xFF, //6
+    (y2 >> 8) & 0xFF, //7
+    (y2 >> 0) & 0xFF, //8
+    trailer0,         //9
+  };
+*/
+//server.write(testdata, sizeof(testdata));

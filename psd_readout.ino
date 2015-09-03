6e1bffae48dbc19774cd3e546c056e787b2f3880
Andrew Peck <anp@ucla.edu>
2015-09-03 12:53:35 -0700

    Add temperature readout

--- psd_readout.ino
+++ psd_readout.ino
@@ -11,7 +11,7 @@ static const int    TRIGf   [8]    = { 41, 50, 34, 39 ,  36, 37, 38, 40 };
 static const int    TRIG_ENABLE [8] = {1,  1,  1,  1,  1,  1,  1,  1}; 
 
 static const int    NSAMPLES = 500;         // Total number of samples to accumulate before printing measurement 500
-static const int    NSAMPLES_PER_CYCLE = 2; // Number of samples to take in a duty cycle 5
+static const int    NSAMPLES_PER_CYCLE = 10; // Number of samples to take in a duty cycle 5
 
 static const float  VREF = 2.5f;  // Analog Voltage Reference
 
@@ -19,6 +19,8 @@ char msg [110];
 bool last_read_state;
 
 volatile bool is_reading = false;
+volatile bool enableA = false; 
+volatile bool enableB = false; 
 volatile char trig_source = 0;
 
 void readSensor(int ipsd, positionMeasurement &data);
@@ -107,40 +109,66 @@ void loop()
 
     if (finished_measuring) {
       // Take average of last NSAMPLES values
-      positionMeasurement difference;
-
+      positionMeasurement difference [2] ;
+      
       for (int ipsd = 0; ipsd < 2; ipsd++) {
-        sum_high[ipsd] = sum_high[ipsd] / NSAMPLES; 
-        sum_low [ipsd] = sum_low [ipsd] / NSAMPLES; 
+        sum_high[ipsd] = sum_high[ipsd]/NSAMPLES; 
+        sum_low [ipsd] = sum_low [ipsd]/NSAMPLES; 
         
-        difference = (sum_high[ipsd] - sum_low[ipsd]); 
-
-        /* Calculated Output */
-        sprintf(msg, "%1i: % 6.4f % 6.4f", ipsd, voltage(double(difference.x2-difference.x1))/voltage(double(difference.x2+difference.x1)), voltage(double(difference.y2-difference.y1))/voltage(double(difference.y2+difference.y1)));
-        Serial.println(msg);
-
-        //sprintf(msg, "%1i: % 6.4f % 6.4f", ipsd, voltage(difference.x()), voltage(difference.y());
-        //Serial.println(msg);
-
-        /* Verbose Output */
+        difference [ipsd] = (sum_high[ipsd] - sum_low[ipsd]); 
+    
+//        /* Verbose Output */
 //        sprintf(msg, "%1i high: x1:%6.4f x2:%6.4f y1:%6.4f y2:%6.4f", ipsd, voltage(sum_high[ipsd].x1), voltage(sum_high[ipsd].x2), voltage(sum_high[ipsd].y1), voltage(sum_high[ipsd].y2));
 //        Serial.println(msg);
 //        sprintf(msg, "%1i  low: x1:%6.4f x2:%6.4f y1:%6.4f y2:%6.4f", ipsd, voltage(sum_low[ipsd].x1), voltage(sum_low[ipsd].x2), voltage(sum_low[ipsd].y1), voltage(sum_low[ipsd].y2));
 //        Serial.println(msg);
 //        sprintf(msg, "temp: %4.1f", readTemperature()); 
 //        Serial.println(msg); 
-      }
+//
+//        /* Calculated Output */
+//        sprintf(msg, "%1i: % 6.4f % 6.4f", ipsd, voltage(double(difference[ipsd].x2-difference[ipsd].x1))/voltage(double(difference[ipsd].x2+difference[ipsd].x1)), 
+//                                                 voltage(double(difference[ipsd].y2-difference[ipsd].y1))/voltage(double(difference[ipsd].y2+difference[ipsd].y1)));
+//        Serial.println(msg);
+
+        }
+        
+        float xa = voltage(difference[0].x2-difference[0].x1)/voltage(difference[0].x2+difference[0].x1);
+        float ya = voltage(difference[0].y2-difference[0].y1)/voltage(difference[0].y2+difference[0].y1);
+        float xb = voltage(difference[1].x2-difference[1].x1)/voltage(difference[1].x2+difference[1].x1);
+        float yb = voltage(difference[1].y2-difference[1].y1)/voltage(difference[1].y2+difference[1].y1);
+
+        // Handle Cases of NAN
+        if (xa != xa) xa = 999.;
+        if (ya != ya) ya = 999.;
+        if (xb != xb) xb = 999.;
+        if (yb != yb) yb = 999.; 
+
+        // Handle Cases Where Only One of the PSDs was triggered
+        if (!enableA) {
+        xa = 999.;
+        ya = 999.;}
+        if (!enableB) {
+        xb = 999.;
+        yb = 999.;
+        }
+        
+        sprintf(msg, "% 9.5f % 9.5f % 9.5f % 9.5f % 5.2f", xa,ya,xb,yb, readTemperature());
+        Serial.println(msg);
+
 
       /* reset */
       memset (count_low,  0, sizeof(count_low [0]) * 2);
       memset (count_high, 0, sizeof(count_high[0]) * 2);
       memset (sum_high,   0, sizeof(sum_high  [0]) * 2);
       memset (sum_low,    0, sizeof(sum_low   [0]) * 2);
+      
+      enableA=false; 
+      enableB=false; 
     }
   }
 }
 
-float voltage (int dac_counts) {
+float voltage (float dac_counts) {
     return ((VREF*dac_counts)/(4095));
  }
 
@@ -153,22 +181,27 @@ void readSensor(int ipsd, positionMeasurement &data)
 }
 
 /* Pin Change Interrupt Routines */
-void interruptA1() { if (TRIG_ENABLE[0]) {trig_source = 0; interrupt();}}
-void interruptA2() { if (TRIG_ENABLE[1]) {trig_source = 1; interrupt();}}
-void interruptA3() { if (TRIG_ENABLE[2]) {trig_source = 2; interrupt();}}
-void interruptA4() { if (TRIG_ENABLE[3]) {trig_source = 3; interrupt();}}
-void interruptB1() { if (TRIG_ENABLE[4]) {trig_source = 4; interrupt();}}
-void interruptB2() { if (TRIG_ENABLE[5]) {trig_source = 5; interrupt();}}
-void interruptB3() { if (TRIG_ENABLE[6]) {trig_source = 6; interrupt();}}
-void interruptB4() { if (TRIG_ENABLE[7]) {trig_source = 7; interrupt();}}
+void interruptA1() { if (TRIG_ENABLE[0]) {trig_source = 0; enableA=true; interrupt();}}
+void interruptA2() { if (TRIG_ENABLE[1]) {trig_source = 1; enableA=true; interrupt();}}
+void interruptA3() { if (TRIG_ENABLE[2]) {trig_source = 2; enableA=true; interrupt();}}
+void interruptA4() { if (TRIG_ENABLE[3]) {trig_source = 3; enableA=true; interrupt();}}
+void interruptB1() { if (TRIG_ENABLE[4]) {trig_source = 4; enableB=true; interrupt();}}
+void interruptB2() { if (TRIG_ENABLE[5]) {trig_source = 5; enableB=true; interrupt();}}
+void interruptB3() { if (TRIG_ENABLE[6]) {trig_source = 6; enableB=true; interrupt();}}
+void interruptB4() { if (TRIG_ENABLE[7]) {trig_source = 7; enableB=true; interrupt();}}
 
 void interrupt()   { if (!is_reading)    {is_reading = true;           }};
 
 float readTemperature() {
-    int   temp        = analogRead(9); 
-    float voltage     = (temp * VREF) /((1<<12)-1); 
-    float temperature = (voltage-0.424)/0.00625;  // conferre the LM60 data sheet for the origin of these magic numbers
-    return temperature; 
+    int temp_measurements = 25; 
+    double temperature_sum=0; 
+    for (int imeas=0; imeas<temp_measurements; imeas++) {
+      int   temp        = analogRead(9); 
+      float voltage     = (temp * VREF) /((1<<12)-1); 
+      float temperature = (voltage-0.424)/0.00625;  // conferre the LM60 data sheet for the origin of these magic numbers
+      temperature_sum += temperature; 
+    }
+    return (temperature_sum/temp_measurements); 
 }
 
 void configureBoard () {

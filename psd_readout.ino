9805985ed49a82e7485f81ee3615cce809634184
Andrew Peck <anp@ucla.edu>
2016-01-12 13:07:58 -0800

    Add serial setting for debug mode. Change PSD trigger state reading to average of analog readings. Decrease number of samples from 10 to 6

--- psd_readout.ino
+++ psd_readout.ino
@@ -10,8 +10,8 @@ static const int    TRIGf   [8]    = { 41, 50, 34, 39 ,  36, 37, 38, 40 };
                                      //A1, A2, A3, A4, B1, B2, B3, B4
 static const int    TRIG_ENABLE [8] = {1,  1,  1,  1,  1,  1,  1,  1}; 
 
-static const int    NSAMPLES = 500;         // Total number of samples to accumulate before printing measurement 500
-static const int    NSAMPLES_PER_CYCLE = 10; // Number of samples to take in a duty cycle 5
+static const int    NSAMPLES = 500;         // Total number of samples to accumulate before printing measurement 
+static const int    NSAMPLES_PER_CYCLE = 6; // Number of samples to take in a duty cycle 
 
 static const float  VREF = 2.5f;  // Analog Voltage Reference
 
@@ -32,9 +32,12 @@ positionMeasurement sum_high[2];
 positionMeasurement sum_low [2];
 
 bool  pinstate = 0;
+bool debug = 0;
+int threshold = 820; 
 
 positionMeasurement voltage_high;
 positionMeasurement voltage_low ;
+
 
 void setup()
 {
@@ -56,23 +59,23 @@ void setup()
 int beat=0; 
 void loop()
 {
-  beat++; 
-  if (beat%1000000==0)     Serial.println("heartbeat..."); 
+  if (debug) {
+    beat++; 
+    if (beat%1000000==0)     
+    Serial.println("heartbeat..."); 
+  }
 
-  if (is_reading) {
+  if (Serial.available() > 0) {
+      char byteIn = Serial.read(); 
+      if      (byteIn == 'd') debug = 1; 
+      else if (byteIn == 'n') debug = 0; 
+  }
+  else if (is_reading) {
     beat=0;
     is_reading = false;
     // wait for signals to rise
-    delayMicroseconds(400);
-
-   //Check that Currently Reading State is opposite of Last Read State (HIGH vs. LOW)
-    bool state = digitalRead(TRIGf[trig_source]);
-
-    if (state == last_read_state) { return; }
-    else                          { last_read_state = state;}
-    digitalWrite(LED[0], state);
-    digitalWrite(LED[1], state);
-
+    delayMicroseconds(300);
+    
     positionMeasurement data [2];
     positionMeasurement sum  [2];
     memset (data, 0, sizeof(data[0]) * 2);
@@ -82,20 +85,38 @@ void loop()
     for (int i=0; i<(2*NSAMPLES_PER_CYCLE); i++) {
       int ipsd = i % 2; // read PSD 0 on even, PSD1 on odd measurements
       readSensor(ipsd, data[ipsd]);
-
       sum[ipsd] = sum[ipsd] + data[ipsd];
     }
 
+    bool state=0; 
+    for (int i=0; i<8; i++) {
+     state |= digitalRead(TRIGf[i]);
+    }
+   
     for (int ipsd = 0; ipsd < 2; ipsd++) {
       data[ipsd] = sum[ipsd] / NSAMPLES_PER_CYCLE;
 
+      bool state=0; 
+      for (int i=0; i<2; i++) {
+        state |= data[ipsd].x1 > threshold; 
+        state |= data[ipsd].x2 > threshold;
+        state |= data[ipsd].y1 > threshold; 
+        state |= data[ipsd].y2 > threshold; 
+      }
+    }
+    
+    //Check that Currently Reading State is opposite of Last Read State (HIGH vs. LOW)
+    if (state == last_read_state) { is_reading=false; return; }
+    else                          { last_read_state = state;}
+    
+    for (int ipsd = 0; ipsd < 2; ipsd++) {
       // Accumulate values for high state
       if (state == HIGH && count_high[ipsd] < NSAMPLES) {
         count_high[ipsd] += 1;
         sum_high[ipsd] = sum_high[ipsd] + data[ipsd];
       }
 
-//      // Accumulate values for low state
+      // Accumulate values for low state
       else if (state == LOW && count_low[ipsd] < NSAMPLES) {
         count_low[ipsd] += 1;
         sum_low[ipsd]    = sum_low[ipsd] + data[ipsd];
@@ -110,51 +131,58 @@ void loop()
     if (finished_measuring) {
       // Take average of last NSAMPLES values
       positionMeasurement difference [2] ;
-      
-      for (int ipsd = 0; ipsd < 2; ipsd++) {
-        sum_high[ipsd] = sum_high[ipsd]/NSAMPLES; 
-        sum_low [ipsd] = sum_low [ipsd]/NSAMPLES; 
-        
-        difference [ipsd] = (sum_high[ipsd] - sum_low[ipsd]); 
-    
-//        /* Verbose Output */
-//        sprintf(msg, "%1i high: x1:%6.4f x2:%6.4f y1:%6.4f y2:%6.4f", ipsd, voltage(sum_high[ipsd].x1), voltage(sum_high[ipsd].x2), voltage(sum_high[ipsd].y1), voltage(sum_high[ipsd].y2));
-//        Serial.println(msg);
-//        sprintf(msg, "%1i  low: x1:%6.4f x2:%6.4f y1:%6.4f y2:%6.4f", ipsd, voltage(sum_low[ipsd].x1), voltage(sum_low[ipsd].x2), voltage(sum_low[ipsd].y1), voltage(sum_low[ipsd].y2));
-//        Serial.println(msg);
-//        sprintf(msg, "temp: %4.1f", readTemperature()); 
-//        Serial.println(msg); 
-//
-//        /* Calculated Output */
-//        sprintf(msg, "%1i: % 6.4f % 6.4f", ipsd, voltage(double(difference[ipsd].x2-difference[ipsd].x1))/voltage(double(difference[ipsd].x2+difference[ipsd].x1)), 
-//                                                 voltage(double(difference[ipsd].y2-difference[ipsd].y1))/voltage(double(difference[ipsd].y2+difference[ipsd].y1)));
-//        Serial.println(msg);
 
+      if (debug) {
+          for (int ipsd = 0; ipsd < 2; ipsd++) {
+          sum_high[ipsd] = sum_high[ipsd]/NSAMPLES; 
+          sum_low [ipsd] = sum_low [ipsd]/NSAMPLES; 
+          
+          difference [ipsd] = (sum_high[ipsd] - sum_low[ipsd]); 
+        
+  
+         
+          /* Verbose Output */
+          sprintf(msg, "%1i high: x1:%6.4f x2:%6.4f y1:%6.4f y2:%6.4f temp:%4.1f", ipsd, voltage(sum_high[ipsd].x1), voltage(sum_high[ipsd].x2), voltage(sum_high[ipsd].y1), voltage(sum_high[ipsd].y2), readTemperature());
+          Serial.println(msg);
+          sprintf(msg, "%1i  low: x1:%6.4f x2:%6.4f y1:%6.4f y2:%6.4f temp:%4.1f", ipsd, voltage(sum_low[ipsd].x1), voltage(sum_low[ipsd].x2), voltage(sum_low[ipsd].y1), voltage(sum_low[ipsd].y2), readTemperature());
+          Serial.println(msg);
+  
+          /* Calculated Output */
+          sprintf(msg, "%1i x: % 6.4f y: % 6.4f", ipsd, voltage(double(difference[ipsd].x2-difference[ipsd].x1))/voltage(double(difference[ipsd].x2+difference[ipsd].x1)), 
+                                                   voltage(double(difference[ipsd].y2-difference[ipsd].y1))/voltage(double(difference[ipsd].y2+difference[ipsd].y1)));
+          Serial.println(msg);
+        
+          } 
         }
+
         
-        float xa = voltage(difference[0].x2-difference[0].x1)/voltage(difference[0].x2+difference[0].x1);
-        float ya = voltage(difference[0].y2-difference[0].y1)/voltage(difference[0].y2+difference[0].y1);
-        float xb = voltage(difference[1].x2-difference[1].x1)/voltage(difference[1].x2+difference[1].x1);
-        float yb = voltage(difference[1].y2-difference[1].y1)/voltage(difference[1].y2+difference[1].y1);
+        else {
+          
+          difference [0] = (sum_high[0] - sum_low[0]);
+          difference [1] = (sum_high[1] - sum_low[1]);
+          float xa = voltage(difference[0].x2-difference[0].x1)/voltage(difference[0].x2+difference[0].x1);
+          float ya = voltage(difference[0].y2-difference[0].y1)/voltage(difference[0].y2+difference[0].y1);
+          float xb = voltage(difference[1].x2-difference[1].x1)/voltage(difference[1].x2+difference[1].x1);
+          float yb = voltage(difference[1].y2-difference[1].y1)/voltage(difference[1].y2+difference[1].y1);
 
-        // Handle Cases of NAN
-        if (xa != xa) xa = 999.;
-        if (ya != ya) ya = 999.;
-        if (xb != xb) xb = 999.;
-        if (yb != yb) yb = 999.; 
+          // Handle Cases of NAN
+          if (xa != xa) xa = 999.;
+          if (ya != ya) ya = 999.;
+          if (xb != xb) xb = 999.;
+          if (yb != yb) yb = 999.; 
 
-        // Handle Cases Where Only One of the PSDs was triggered
-        if (!enableA) {
-        xa = 999.;
-        ya = 999.;}
-        if (!enableB) {
-        xb = 999.;
-        yb = 999.;
-        }
+          // Handle Cases Where Only One of the PSDs was triggered
+          if (!enableA) {
+            xa = 999.;
+            ya = 999.;}
+          if (!enableB) {
+            xb = 999.;
+            yb = 999.;
+          }
         
-        sprintf(msg, "% 9.5f % 9.5f % 9.5f % 9.5f % 5.2f", xa,ya,xb,yb, readTemperature());
-        Serial.println(msg);
-
+          sprintf(msg, "% 9.5f % 9.5f % 9.5f % 9.5f % 5.2f", xa,ya,xb,yb, readTemperature());
+          Serial.println(msg);
+      }
 
       /* reset */
       memset (count_low,  0, sizeof(count_low [0]) * 2);
@@ -169,7 +197,7 @@ void loop()
 }
 
 float voltage (float dac_counts) {
-    return ((VREF*dac_counts)/(4095));
+    return ((VREF*dac_counts)/(2*4095));
  }
 
 void readSensor(int ipsd, positionMeasurement &data)
@@ -190,7 +218,7 @@ void interruptB2() { if (TRIG_ENABLE[5]) {trig_source = 5; enableB=true; interru
 void interruptB3() { if (TRIG_ENABLE[6]) {trig_source = 6; enableB=true; interrupt();}}
 void interruptB4() { if (TRIG_ENABLE[7]) {trig_source = 7; enableB=true; interrupt();}}
 
-void interrupt()   { if (!is_reading)    {is_reading = true;           }};
+void interrupt()   { if (!is_reading)    {is_reading = true;}};
 
 float readTemperature() {
     int temp_measurements = 25; 
